044ba36f5c4f6a967c7c8038ef5b898f84fdc097
Martin Leibner <tincorad@gmail.com>
2015-02-28 21:05:27 -0200

    Agregando comentarios y constantes

--- ui/neosfinder.py
+++ ui/neosfinder.py
@@ -2,12 +2,27 @@
 from gi.repository import Gtk
 from PrincipalHandler import Handler
 
+ARCHIVO_UI = "interfaz.glade"
+ID_VENTANA = "AppWinPrincipal"
+SEÑAL_CIERRA_VENTANA = "delete-event"
+
+# Crea una instancia de Builder en builder
 builder = Gtk.Builder()
-builder.add_from_file("interfaz.glade")
+
+# Carga la definicion de la interfaz de usuario del archivo xml de Glade
+builder.add_from_file(ARCHIVO_UI)
+
+# Conecta las 'señales' (eventos) para que vinculen a los métodos declarados en la clase Handler
 builder.connect_signals(Handler())
 
-window = builder.get_object("AppWinPrincipal")
-window.connect("delete-event", Gtk.main_quit)
+# Crea una instancia window a partir del objeto con id en builder
+window = builder.get_object(ID_VENTANA)
+
+# Conecta la señal "delete_event" al metodo Gtk.main_quit que cierra el loop Gtk principal
+window.connect(SEÑAL_CIERRA_VENTANA, Gtk.main_quit)
+
+# Hace visibles todos los objetos mostrables hijos de window incluido si mismo
 window.show_all()
 
+# Inicia y mantiene corriendo el loop principal de la UI. Sale en main_quit
 Gtk.main()
