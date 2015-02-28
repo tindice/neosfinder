da434d29963b4f6106f840d40ef97cbaf5b3416b
Martin Leibner <tincorad@gmail.com>
2015-02-28 16:39:32 -0200

    Separando la clase de la ventana principal a su propio archivo.

--- ui/neosfinder.py
+++ ui/neosfinder.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python
 from gi.repository import Gtk
-
-class PrincipalWindow(Gtk.Window):
-    
-    # Declaramos constantes y preferencias de configuracion
-    ANCHO_VENTANA = 640
-    ALTO_VENTANA = 480
-    global TITULO = "NeosFinder v0.1"
-    
-    def __init__(self):
-        ANCHO_VENTANA = 640
-        ALTO_VENTANA = 480
-        #~ TITULO = "NeosFinder v0.1"
-        Gtk.Window.__init__(self, title=TITULO, default_width=ANCHO_VENTANA, default_height=ALTO_VENTANA)
-        
-        self.button = Gtk.Button(label="Cliqueame :-)")
-        self.button.connect("clicked", self.on_button_clicked)
-        self.add(self.button)
-        #~ self.default_height = 200
-
-    def on_button_clicked(self, widget):
-        print("Hola mundo UI!!")
+from PrincipalWindow import PrincipalWindow
 
 win = PrincipalWindow()
 win.connect("delete-event", Gtk.main_quit)
