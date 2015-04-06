511d9b5dabe1bca611aa4f4e504ae8057cf87f43
Rodolfo Leibner <rleibner@gmail.com>
2015-04-05 21:11:22 -0300

    sliders

--- ui/nfClass.py
+++ ui/nfClass.py
@@ -13,9 +13,40 @@ class Gui:
     Gtk.main_quit()
 
   def on_mnuEqualize(self, menuitem, data=None):
-    print "help about selected"
-    self.response = self.equadialog.run()
+    # valores por defecto:
+    amin, amax = np.amin(self.data), np.amax(self.data)
+    delta = amax-amin
+    i0, i1 = amin+ 0.0185*delta, amin+ 0.0323*delta
+    self.info.set_property("label","min=%i   max=%i"%(amin,amax))
+    self.adjmin.set_property("upper", 1.25 * delta/256)
+    self.adjmax.set_property("upper", 1.25 * delta/256)
+    self.adjmin.set_property("value", i0/256)
+    self.adjmax.set_property("value", i1/256)
+    self.chkauto.set_property("active", True)
+    self.equadialog.run()
     
+  def on_dlgEqualize_response(self, menuitem, data=None):
+    if data == 1:
+        pass
+    else:
+        self.equadialog.hide()
+        
+  def on_chkAuto_toggled(self, menuitem, data=None):
+    if self.chkauto.get_property("active"):
+        self.info.set_property("label","activo")
+        self.adjmin.set_property("value", self.automin)
+        self.adjmax.set_property("value", self.automax)
+          
+  def on_adjmin_value_changed(self, menuitem, data=None):
+    if self.chkauto.get_property("active"):
+        self.chkauto.set_property("active", False)
+        self.automin = self.adjmin.get_property("value")
+
+  def on_adjmax_value_changed(self, menuitem, data=None):
+    if self.chkauto.get_property("active"):
+        self.chkauto.set_property("active", False)
+        self.automax = self.adjmax.get_property("value")
+
   def on_mnuAcercaDe_activate(self, menuitem, data=None):
     print "help about selected"
     self.response = self.aboutdialog.run()
@@ -30,16 +61,19 @@ class Gui:
   def on_fitchooserdialog_response(self, menuitem, data=None):
     if data == 1:
         self.fitlist = self.fitchooser.get_filenames()
-        _, data = Getdata(self.fitlist[0])
-        png = Fit2png(data)
+        # display imagen 1:
+        _, self.data = Getdata(self.fitlist[0])
+        png = Fit2png(self.data)
         size = (png.shape[1]/2, png.shape[0]/2)
         im = Image.fromarray(png)
         im.thumbnail(size, Image.BICUBIC)
         im.save("../tmp/tmp.png")
-
-        #~ pixbuf = GdkPixbuf.Pixbuf.new_from_array(png)
+# TODO: 
+        #~ pixbuf = Gtk.Gdk.pixbuf_new_from_array(np.array(im),Gtk.gdk.COLORSPACE_RGB,8)
+        #~ pixbuf = GdkPixbuf.pixbuf_new_from_array(np.array(im),Gtk.gdk.COLORSPACE_RGB,8)
         pixbuf = GdkPixbuf.Pixbuf.new_from_file('../tmp/tmp.png')
         self.imagen.set_property("pixbuf", pixbuf)
+        self.info.set_property("label",self.fitlist[0])
     else:
         print "Cancel"
     self.fitchooser.hide()
@@ -57,6 +91,11 @@ class Gui:
     self.equadialog = self.builder.get_object("dlgEqualize")
     self.fitlist = []
     self.imagen = self.builder.get_object("imagen")
+    self.histogram = self.builder.get_object("drawingarea")
+    self.info = self.builder.get_object("infolbl")
+    self.adjmin = self.builder.get_object("adjmin")
+    self.adjmax = self.builder.get_object("adjmax")
+    self.chkauto = self.builder.get_object("chkAuto")
     self.window.show()
     
 #~ print fitlist
